2311d0f9ecafd837d84b87b3d7debf304b78a76f
ismail <ismail.buev@mail.ru>
2021-03-21 20:44:19 +0300

    JS 6

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,65 +1,62 @@
-//Функция filterNegative() принимает массив с любыми числами и возвращает массив состоящий только из положительных.
-let arrNum = [123, -13, 0, 1474, 1, -312, -2];
-function filterNegative(numbers) {
-    let arr = [];
-    for( i=0; i < numbers.length; i++){  
-        if(numbers[i] >= 0) {
-            arr.push(numbers[i])
-        }
-    };
-    return arr;
-};
-filterNegative(arrNum);
-// console.log(filterNegative(arrNum)); // выводим функцию в консоль.
+//forEach
+//Дан массив с числами [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100]. Вывести в консоль только нечетные числа.
+//(Для этого вам необходимо использовать if и узнать оператор про оператор ‘остаток от деления’ - % 
 
-//Функция filterString() принимает массив с любыми типами элементов и возвращает массив состоящий только из строчных элементов.
-let NumStr = [123, "lorem", 0, 1474, "Ipsum", "Num", 10];
-function filterString(string){
-    let str= [];
-    for( i=0; i < string.length; i++){
-        if(typeof string[i] == "string"){
-            str.push(string[i]);
-        }
-    };
-    return str;
-}
-filterString(NumStr);
-//  console.log(filterString(NumStr)); // выводим функцию в консоль.
+// let oddNumber=[10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100];
+// oddNumber.forEach(function (num){
+//     if(num % 2 == 0){
+//         console.log(num);
+//     }
+// });
 
+//Дан массив со строчными элементами [ “lorem ipsum”, “javascript”, “php”, “css”, “react”, “git”, “html”, “mysql”].
+// Вывести в консоль элементы у которых кол-во символов не больше 4-х. 
 
-//Функция filterLength() принимает два параметра: первый - массив с текстовыми элементами, второй - число.
-// Функция должна вернуть только те элементы длина строки которых не больше, чем второй параметр.
-let three=["js", "php", "react", "css"];
-function filterLength(str, num){
-    let arr=[];
-    for(i=0; i < str.length; i++){
-        if(str[i].length<=num){
-            arr.push(str[i]);
-        }   
-    };
-    return arr;
-};
-filterLength(three, 3);
-// console.log(filterLength(three, 3)); // выводим функцию в консоль.
+// let strArr=["lorem ipsum","javascript","php","css","react","git","html","mysql"];
+// strArr.forEach(function (item){
+//     if(item.length <= 4){
+//         console.log(item);
+//     }
+// });
 
-// 2 Создать функцию myMap(), которая принимает один параметр числовой массив и возвращает новый массив 
-// увеличивая элементы старого в два раза.
-let numMultiply=[12, 2, 3, 1, 10, 150];
-function myMap(num){
-    let arr=[];
-    for( i=0; i < num.length; i++){       
-        arr.push(num[i]*2);
-    };
-    return arr;
-};
-myMap(numMultiply);
-// console.log(myMap(numMultiply)); // выводим функцию в консоль.
+//filter
+//Дан массив [1, -3, 5, 6, -7, 8, 9, -11]. Создайте новый массив в котором останутся только отрицательные элементы. 
+let numbr=[1, -3, 5, 6, -7, 8, 9, -11];
+let negNumbers = numbr.filter(function(item){
+    return item < 0;
+});
+// console.log(negNumbers); // выводим в консоль
 
-//3 Создать функцию reverseWords() которая принимает один параметр - строку. Функция должна вернуть строку в обратно порядке.
-function reverseWords(string){
-    arrString=string.split(" ");
-    return arrString.reverse();
-};
-reverseWords("yoda doesn't speak like this");
-// console.log(reverseWords("yoda doesn't speak like this")); // выводим функцию в консоль.
+//Дан массив [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100]. Создать новый в котором будут только четные элементы этого массива. 
+//(Нужен оператор остаток от деления - %). 
 
+let numbr2=[10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100];
+let evenNumbers = numbr2.filter(function(item){
+    return item % 2 == 0;
+});
+//console.log(evenNumbers); // выводим в консоль
+
+//Дан массив [ “lorem ipsum”, “javascript”, “php”, “css”, “react”, “git”, “html”, “mysql”] 
+//создать новый массив состоящий только из элементов у которых длина строки не равна 3-м. 
+
+let strArr2=["lorem ipsum","javascript","php","css","react","git","html","mysql"];
+let strlngNo3 = strArr2.filter(function(item){
+    return item.length != 3;
+});
+// console.log(strlngNo3); // выводим в консоль
+
+//Map
+//Дан массив [5, 6, 7, 8, 9], сделайте из него массив состоящий из квадратов этих числе. (25, 36.. и тд)
+let numbr3=[5, 6, 7, 8, 9];
+let square=numbr3.map(function(item){
+    return item * item;
+});
+// console.log(square); // выводим в консоль
+
+//Дан массив с объектами [{а: 10, b: 5}, {а: 20, b: 22}, {a: 131, b:55}]. 
+//Создать новый массив элементами которого буду сумма свойств объекта. Должно получится [15, 42, 186]
+let arrObj=[{a: 10, b: 5}, {a: 20, b: 22}, {a: 131, b:55}];
+let newArrObj=arrObj.map(function(item){
+    return item.a+item.b;
+});
+// console.log(newArrObj); // выводим в консоль;
